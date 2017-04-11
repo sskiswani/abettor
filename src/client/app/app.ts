1e1b1bb57f4c31630d587baa21e5dfb842d9a8c6
name <s@example.com>
2017-04-11 12:55:38 -0500

    routes and reduce the number of files

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -1,5 +1,6 @@
 import * as angular from 'angular';
 import * as uiRouter from 'angular-ui-router';
+import { IStateProvider, IUrlRouterProvider } from 'angular-ui-router';
 import AppConfig from './app.config';
 import AppController from './app.controller';
 import './app.scss';
@@ -9,15 +10,29 @@ import Markdown from './common/modules/markdown';
 import { LocalStorageProvider } from './services/LocalStorageProvider';
 
 const app = angular
-   .module('app', [uiRouter, Markdown, common])
+   .module('app', [(<string><any>uiRouter), Markdown, common])
+   .config((config, $stateProvider: IStateProvider, $urlRouterProvider: IUrlRouterProvider) => {
+      $stateProvider.state({
+         name: 'todo',
+         url: '/todo',
+         template: '<h1>TODO</h1>'
+      });
+
+      $stateProvider.state('home', {
+         url: '',
+         template: '<h1>welcome home</h1>'
+      });
+
+      $urlRouterProvider.otherwise('/');
+   })
    .constant('config', AppConfig)
    .provider('$localStorage', LocalStorageProvider)
    .controller('appController', AppController)
-   .component('app', {
-      replace: true,
+   .directive('app', () => ({
+      restrict: 'AE',
       template: require<string>('./app.html'),
       controller: AppController
-   } as any);
+   }));
 
 export { app }
 export default (app.name);
